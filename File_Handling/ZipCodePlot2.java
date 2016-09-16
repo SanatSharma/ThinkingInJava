7e5b9a4ace67e59e44d7a1d1388909cc7a21161c
SanatSharma <sanatcolumban@gmail.com>
2016-09-15 23:05:54 -0500

    New Test Code
    Added some sample demo classes I created recently

---
+++ File_Handling/ZipCodePlot2.java
@@ -0,0 +1,117 @@
+package File_Handling;
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Scanner;
+import java.util.Random;
+import java.io.File;
+import java.io.IOException;
+
+// display zip code locations
+// file format is for zipcode.csv is:
+// zip  city    state   latitude    longitude   timezone    dst
+// // lattitude is the 4th field and longitude is the 5th field
+// 
+// File format for zipCode_49.csv is:
+// Zipcode  ZipCodeType City    State   LocationType    Lat Long    Location    Decommisioned   TaxReturnsFiled EstimatedPopulation TotalWages
+// lattitude is the 6th field and longitude is the 7th field
+
+public class ZipCodePlot2 {
+
+    public static final String DATA_FILE = "zipcode.csv";
+
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 700;
+
+    public static final int TOKENS_BEFORE_LAT_LONG = 3;
+    public static final int TOKENS_BEFORE_STATE = 2;
+    
+    public static void main(String[] args ) {
+        Scanner fileScanner = createScanner();
+        double minLat = 90;
+        double maxLat = -90;
+        double minLong = 180;
+        double maxLong = -180;
+        while(fileScanner.hasNextLine()) {
+            Scanner lineScanner = new Scanner(fileScanner.nextLine());
+            lineScanner.useDelimiter(",");
+            skipTokens(lineScanner, TOKENS_BEFORE_LAT_LONG);
+            double lat = lineScanner.nextDouble();
+            double longi = lineScanner.nextDouble();
+            minLat = Math.min(minLat, lat);
+            maxLat = Math.max(maxLat, lat);
+            minLong = Math.min(minLong, longi);
+            maxLong = Math.max(maxLong, longi);   
+        }
+        System.out.println(minLat + " " + maxLat + " " + 
+            minLong + " " + maxLong);
+
+        drawZips(minLat, maxLat, minLong, maxLong);
+    }
+
+    public static Scanner createScanner() {
+        try {
+            Scanner sc = new Scanner(new File(DATA_FILE));
+            // skip the first line
+            sc.nextLine();
+            return sc;
+        }
+        catch(IOException e) {
+            System.out.println("Unable to create scanner. Program likely to crash.");
+            return null;
+        }
+    }
+
+    // skip the first 5 tokens in the line
+    public static void skipTokens(Scanner sc, int tokensToSkip) {
+        for(int i = 0; i < tokensToSkip; i++) {
+            sc.next();
+        }
+    }
+
+    public static void drawZips(double minLat, double maxLat, 
+    double minLong, double maxLong)  {
+
+        DrawingPanel p = new DrawingPanel(WIDTH, HEIGHT);
+        p.setBackground(Color.BLACK);
+        Graphics g = p.getGraphics();
+        g.setColor(Color.WHITE);
+
+        double pixelsPerDegreeLat = HEIGHT / (maxLat - minLat);
+        double pixelsPerDegreeLong = WIDTH / (maxLong - minLong);
+
+        Scanner fileScanner = createScanner();
+        
+        Random rand = new Random();
+        String tempState = "";
+
+        while(fileScanner.hasNextLine()) {
+            Scanner lineScanner = new Scanner(fileScanner.nextLine());
+            lineScanner.useDelimiter(",");
+            skipTokens(lineScanner, TOKENS_BEFORE_STATE);
+            String state = lineScanner.next();  
+            double lat = lineScanner.nextDouble();
+            double longi = lineScanner.nextDouble();
+            
+            
+
+            if(!tempState.equals(state)){
+                int r = rand.nextInt(200)+56;
+                int gr = rand.nextInt(200)+56;
+                int b = rand.nextInt(200)+56;
+                g.setColor(new Color(r,gr,b));
+                if(state.equals("TX")){
+                    g.setColor(new Color(191,87,0));
+                }
+            }
+            
+            tempState= state;
+
+            int x = (int) (pixelsPerDegreeLong * (longi - minLong));
+            int y = (int) (pixelsPerDegreeLat * (lat - minLat));
+            y = HEIGHT - y;
+            g.fillRect(x, y, 1, 1);
+        }
+    }
+}
---
+++ Trees/BinaryTreeCS314Spring2015Paper.java
@@ -0,0 +1,5 @@
+package Trees;
+
+public class BinaryTreeCS314Spring2015Paper {
+
+}
